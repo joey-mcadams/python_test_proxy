58e5024509cb89fa6d532564b371237d8d037221
Joey McAdams <joey.mcadams@hotmail.com>
2021-08-08 14:04:31 -0500

    documentation

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # python_test_proxy
-A generic proxy server written in python. 
+A generic proxy server written in python.
+
+This is some scratch / garbage code. It's a really fast implementation of a Flask proxy server I wanted to test out.
+
+This will look for any incoming payload that has: 
+
+```
+{
+    "test_key":"malicious"
+}
+```
+
+If that key is found, it will respond with a 451 (Legally unable to respond) error. 
+
+The requirements are pinned due to a bug in the Werkzeug library. If you use the latest versions, you can not 
+debug this app in pycharm.
+
---
+++ proxy_main.py
@@ -0,0 +1,45 @@
+"""
+Mostly taken from
+https://medium.com/@zwork101/making-a-flask-proxy-server-online-in-10-lines-of-code-44b8721bca6
+"""
+
+import flask
+from flask import Flask, request, Response
+from requests import get
+import json
+
+app = Flask(__name__)
+SITE_NAME = 'https://google.com/'
+
+
+def pre_process_request(request_: flask.Request) -> bool:
+    # noinspection PyBroadException
+
+    try:
+        payload_string = request_.data.decode()
+        payload = json.loads(payload_string)
+        if "test_key" in payload.keys():
+            if payload.get("test_key", None) == "malicious":
+                return False
+
+    # This is terrible practice, to broad exception.
+    # However, I didn't want to deal with the all the exceptions that can happen
+    # if the payload isn't actually JSON or even text
+    except Exception as e:
+        pass
+
+    return True
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def proxy(path):
+    if pre_process_request(request):
+        return get(request.url).content
+    else:
+        return Response("Malicious JSON", 451)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080)
